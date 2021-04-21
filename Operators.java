a9a0a602d1a583f6843e6e6b70581055c5dc98d4
Mshaveta <Shaveta.malhotra47@gmail.com>
2021-04-21 21:58:52 +0530

    Done

--- Operators.java
+++ Operators.java
@@ -6,13 +6,94 @@ public class Operators{
 		a++; //11-->12
 		System.out.println(a);//12 */
 		
-		int b = 5;
+		/* int b = 5;//5
 		int c = b++;//c=5,b=5-->6 in m/m
-		b++;//7
-		b++;//8
-		System.out.println(b);8
-		System.out.println(c);//5
+		 
+		System.out.println(b);//6
+		System.out.println(c);//5 */
 		/////////////////////////////////////
+		
+		// int a= 10;//11-->12
+		// System.out.println(a++);//10---> 10 with 11
+		// System.out.println(a);//11
+		
+		// int c= (a++) + (a);//11 + 12 =23
+		// System.out.println(c); //23
+		// System.out.println(a);//12
+		
+		////////////////////////////////////
+		/* int b=2;
+		b++;//3
+		b++;//4-->5
+		int e=1;
+		int d = b + b++ +e;//4  + 4 +1 =9
+		System.out.println(b); 
+		System.out.println(d); 
+		System.out.println(e); //1
+		 */
+		
+		/////////////////////////////////////
+		//post ++, pre
+		/* int b=2; //3//4
+	 
+		
+		System.out.println(b++); //2
+		
+		System.out.println(b); //3
+		 
+		int c= b++;//c=3-->4
+		
+		System.out.println(b); //4
+		//in m/m---4->5->6
+		int d = b++ + b + b++;// d = 4 + 5 + 5 = 14
+		System.out.println(d); //14
+		System.out.println(b); //6 */
+		
+		/* int a=1;
+		int b=2;
+		int c = a+b ; //1+2=3
+		c++; //4
+		++c;//5
+		//a=1-->2,b=2-->3,c=5-->6
+		
+		int d = a++ + ++b + ++c + c ;//d = 1 +  3 +  6 + 6 = 16
+		
+		System.out.println(a); //2
+		System.out.println(b);//3
+		System.out.println(c); //6
+		System.out.println(d); //16 */
+		
+		
+		//swapping of two numbers using third variable
+		
+		int a=10;//20
+		int b=20;
+		
+		System.out.println("Before Swapping-"+a+","+b); 
+		 
+		int temp = a;//10
+		
+		
+		a=b;//Assiging the value of b -20to a //a=20
+		b=temp//Assiging the value of temp to b//10
+		
+		System.out.println("After Swapping-"+a+","+b); //
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
---
+++ Operators_Arth.java
@@ -0,0 +1,32 @@
+/*
+1. Sum of digits- 123 - 1+2+3= 6
+2. reverse of a anumber--234 --432
+3. pallindrom 121,111,1331-----121
+4. Armstrong or not - 153--1^3 +  5^3 +3^3 === 153
+
+*/
+public class Operators_Arth{
+	public static void main(String[] args){
+		// int a=10;
+		// int b=2;
+		// int c = a%b;//0
+		// int d = a/b;//5
+		// System.out.println(c);
+		// System.out.println(d);
+		//451 --> 4 5 1
+		int a=457;//45//4 5 1 -- 145
+		int r1 = a%10;//7 
+		a = a/10;//45
+		
+		//for scnd digit
+		int r2 = a%10;//45%10 -->5
+		
+		//third digit
+		int r3 =a/10;//45/10--->4
+		
+		System.out.println(r1+r2+r3); //7+5+4 = 16
+		 
+		
+		 
+	}
+}
